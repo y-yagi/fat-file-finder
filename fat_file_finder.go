f20d473040c341be53db20e908dc47855b055574
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-21 17:57:50 +0900

    use more goroutine

--- fat_file_finder.go
+++ fat_file_finder.go
@@ -3,34 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"code.cloudfoundry.org/bytefmt"
 )
 
-func search(location string, thresholdSize int64) chan string {
-	chann := make(chan string)
+func search(location string, thresholdSize int64, wg *sync.WaitGroup) {
+	err := filepath.Walk(location, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 
-	go func() {
-		_ = filepath.Walk(location, func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				chann <- fmt.Sprintf("%v", err)
-				return nil
-			}
-
-			if info.IsDir() {
-				return nil
-			}
-
-			if info.Size() > thresholdSize {
-				chann <- fmt.Sprintf("%s (%s)", path, bytefmt.ByteSize(uint64(info.Size())))
-			}
+		if info.IsDir() {
 			return nil
-		})
-		defer close(chann)
-	}()
-	return chann
+		}
+
+		if info.Size() > thresholdSize {
+			fmt.Printf("%s (%s)\n", path, bytefmt.ByteSize(uint64(info.Size())))
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	wg.Done()
 }
 
 func main() {
@@ -39,6 +39,7 @@ func main() {
 	var fileSizeStr string
 	var location string
 	var showVersion bool
+	var wg sync.WaitGroup
 
 	flag.StringVar(&fileSizeStr, "s", "100M", "Threshold size to display.")
 	flag.StringVar(&location, "l", ".", "Search location.")
@@ -58,8 +59,21 @@ func main() {
 		return
 	}
 
-	chann := search(location, int64(thresholdSize))
-	for msg := range chann {
-		fmt.Println(msg)
+	fileInfos, err := ioutil.ReadDir(location)
+	if err != nil {
+		fmt.Printf("Location is invalid value. %v\n", err)
+		os.Exit(1)
+		return
 	}
+
+	for _, fileInfo := range fileInfos {
+		fullPath := filepath.Join(location, fileInfo.Name())
+		if fileInfo.IsDir() {
+			wg.Add(1)
+			go search(fullPath, int64(thresholdSize), &wg)
+		} else if fileInfo.Size() > int64(thresholdSize) {
+			fmt.Printf("%s (%s)\n", fullPath, bytefmt.ByteSize(uint64(fileInfo.Size())))
+		}
+	}
+	wg.Wait()
 }
