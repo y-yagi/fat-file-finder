ec7f25eb3ff736bb7ce91337df64191309884ec4
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-12-10 08:28:16 +0900

    fix broken test

--- fat_file_finder_test.go
+++ fat_file_finder_test.go
@@ -12,7 +12,7 @@ func TestSearch(t *testing.T) {
 	out := new(bytes.Buffer)
 	search("testdata", int64(thresholdSize), out)
 
-	expected := "testdata/1m (1000K)\n"
+	expected := "f testdata/1m (1000K)\nd testdata/ (1000K)\n"
 	if out.String() != expected {
 		t.Fatalf("expected '%s' but '%s'", expected, out.String())
 	}
